8255764bdcd9ef54f98b946c4759900fbc0fe2ef
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-08-26 11:48:37 -0400

    bump to 0.0.2

--- lib/redis/orm/version.rb
+++ lib/redis/orm/version.rb
@@ -3,7 +3,7 @@ class Redis
     module Version
       MAJOR = 0
       MINOR = 0
-      PATCH = 1
+      PATCH = 2
       PREREL = nil
     
       STRING = PREREL ? [MAJOR, MINOR, PATCH, PREREL].join('.') : [MAJOR, MINOR, PATCH].join('.')
